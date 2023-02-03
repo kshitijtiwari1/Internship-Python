c9e752bdb29e55a76da82fa24ae0524e6aa63fac
Kshitij Tiwari <62513825+kshitijtiwari1@users.noreply.github.com>
2023-02-03 11:54:54 +0530

    Add files via upload

---
+++ 1sum.py
@@ -0,0 +1,5 @@
+#write a program to print sum of 2 numbers and display it
+
+n1 = int(input("Enter a number:"))
+n2 = int(input("Enter another number:"))
+print("Sum of the numbers is:" , n1+n2)
---
+++ 2difference.py
@@ -0,0 +1,5 @@
+#write a program to find difference of 2 numbers
+
+n1 = int(input("Enter a number:"))
+n2 = int(input("Enter another number:"))
+print("Difference of the numbers is:" , n1-n2)
