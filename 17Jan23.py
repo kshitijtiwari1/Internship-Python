cb48a4fa617c8f00ea366c8390d1805c0bb8c8b9
Kshitij Tiwari <62513825+kshitijtiwari1@users.noreply.github.com>
2023-02-13 19:26:07 +0530

    Add files via upload

---
+++ 17Jan23.py
@@ -0,0 +1,16 @@
+# class Student:
+#     school_name = 'DPS'
+    
+#     def __init__(self, name, id, score):
+#         self.name = name
+#         self.id = id
+#         self.score = score
+
+#     def display_student(self):
+#         print(self.name , self.id, self.school_name , self.score)
+        
+# student_1 = Student('kshitij', 12, 51) 
+# student_1.display_student()
+
+# student_2 = Student('aman', 24, 78)
+# student_2.display_student() 
