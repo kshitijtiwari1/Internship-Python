80d9077dc777739ad0118902f67af99a97d13668
Kshitij Tiwari <62513825+kshitijtiwari1@users.noreply.github.com>
2023-02-14 17:52:50 +0530

    Add files via upload

---
+++ 19Jan23.py
@@ -0,0 +1,12 @@
+# # while True:
+# #     try:
+# #         a = int(input("Input a number: "))
+# #         break
+# #     except ValueError:
+# #         print("Invalid input: not an integer")
+        
+# x = int(input("Enter a number: "))
+# try:
+#     print(x/0)
+# except ZeroDivisionError:
+#     print("zero division error occured")
